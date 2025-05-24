b1fbb50ddce27e77e6414ec060c642d50def5553
taz-szk <tkht.szk.1801@gmail.com>
2025-05-24 11:25:24 +0900

    コンポーネントリスト追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { navbarComponent } from "./common/navbar/navbar.component";
+import { productListComponent } from "./product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, navbarComponent],
+  imports: [RouterOutlet, navbarComponent, productListComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
